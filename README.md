094aafff940e6aaf661cf1a84c953cb7b45ad4b2
accult <acculltt@gmail.com>
2024-03-28 21:46:44 +0200

    feat(almost-done): almost done

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# Test task 2
+# Click Tracker
 
-What's wrong with me.....
+Click Tracker is a Django application designed to track user clicks on a link. It records user data such as IP address, browser, and the time of the click in a database. This project allows you to implement a server backend that processes GET requests, redirects users to another page, and stores their data.
+
+## Getting Started
+
+To get started with Click Tracker, follow these steps:
+
+1. Clone the repository: `git clone <repository_url>`
+2. Navigate to the project directory: `cd click_tracker`
+3. Create a virtual environment: `python3 -m venv venv`
+4. Activate the virtual environment:
+   - On macOS/Linux: `source venv/bin/activate`
+   - On Windows: `venv\Scripts\activate`
+5. Install Django and other dependencies: `pip install -r requirements.txt`
+6. Create a `.env` file in the project directory.
+7. Customize the .env file using the template from .env-example
+8. Migrate the database: `python manage.py migrate`
+9. Run the development server: `python manage.py runserver`
+
+
+Enjoy your usage 🙃
--- click_tracker/settings.py
+++ click_tracker/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hz&3uk%+gep0@#q!y71bnlhr6@3a2l@xqt&fk79)wrdtupq1e5"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -78,11 +82,11 @@ WSGI_APPLICATION = "click_tracker.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "click_tracker",
-        "USER": "admin",
-        "PASSWORD": "mypassword",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
